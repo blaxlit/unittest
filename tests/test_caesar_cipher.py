61e0048e1ac7d035d75bc7f2782d0fdd45017cbc
Thaweerat <thaweeratyordsathien@gmail.com>
2026-02-18 14:29:41 +0700

    Fix Caesar Cipher logic and organize Grid Challenge tests

--- tests/test_caesar_cipher.py
+++ tests/test_caesar_cipher.py
@@ -1,14 +1,11 @@
 import unittest
-from coe_number import grid_challenge
 from coe_number.caesar_cipher import caesar_cipher
 
 class CaesarCipherTest(unittest.TestCase):
-    def test_give_Always_Look_k2_should_be_Cnways_Nqqm(self):
-        s = "Always-Look"
-        k = 2
+    def test_give_Always_Look_k2_should_be_Cnycau_Nqqm(self):
+        s, k = "Always-Look", 2
         result = caesar_cipher(s, k)
-        self.assertEqual(result, "Cnways-Nqqm")
+        self.assertEqual(result, "Cnycau-Nqqm")
+
     def test_give_159357l_k2_should_be_159357n(self):
         self.assertEqual(caesar_cipher("159357l", 2), "159357n")
-    def test_give_grid_not_sorted_should_be_no(self):
-        self.assertEqual(grid_challenge(["cba", "daf", "ghi"]), "NO")
--- tests/test_grid_challenge.py
+++ tests/test_grid_challenge.py
@@ -3,4 +3,9 @@ from coe_number.grid_challenge import grid_challenge
 
 class GridChallengeTest(unittest.TestCase):
     def test_give_grid_should_be_yes(self):
-        self.assertEqual(grid_challenge(["ebacd", "fghij", "olmkn", "trpqs", "xywuv"]), "YES")
+        self.assertEqual(grid_challenge(["abc", "ade", "efg"]), "YES")
+
+    def test_give_grid_not_sorted_should_be_no(self):
+        grid = ["cba", "daf", "ghi"]
+        result = grid_challenge(grid)
+        self.assertEqual(result, "NO")
