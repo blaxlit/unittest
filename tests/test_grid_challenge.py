c8073caf5fa77f1ae9c5487ccea68f9be799199e
Thaweerat <thaweeratyordsathien@gmail.com>
2026-02-18 14:32:50 +0700

    Fix Grid Challenge test case and finalize project

--- tests/test_grid_challenge.py
+++ tests/test_grid_challenge.py
@@ -3,9 +3,11 @@ from coe_number.grid_challenge import grid_challenge
 
 class GridChallengeTest(unittest.TestCase):
     def test_give_grid_should_be_yes(self):
-        self.assertEqual(grid_challenge(["abc", "ade", "efg"]), "YES")
+        grid = ["eabcd", "fghij", "olkmn", "trpqs", "xywuv"]
+        result = grid_challenge(grid)
+        self.assertEqual(result, "YES")
 
     def test_give_grid_not_sorted_should_be_no(self):
-        grid = ["cba", "daf", "ghi"]
+        grid = ["mpxz", "abcd", "wlmf"] 
         result = grid_challenge(grid)
         self.assertEqual(result, "NO")
